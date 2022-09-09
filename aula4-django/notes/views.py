115f6ea968aa4d5f2a8b76b67289fd3b256c3324
Enricco Gemha <riccogemha@gmail.com>
2022-09-09 08:35:46 -0300

    handout 4 OK

--- aula4-django/notes/views.py
+++ aula4-django/notes/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Note
 
 
 def index(request):
-    all_notes = Note.objects.all()
-    print(all_notes)
-    return render(request, 'notes/index.html', {'notes': all_notes})
+    if request.method == 'POST':
+        title = request.POST.get('titulo')
+        content = request.POST.get('detalhes')
+        # TAREFA: Utilize o title e content para criar um novo Note no banco de dados
+        note = Note.objects.create(title=title, content=content)
+        note.save()
+        return redirect('index')
+    else:
+        all_notes = Note.objects.all()
+        return render(request, 'notes/index.html', {'notes': all_notes})
